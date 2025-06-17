1abfe154583acb1d50ec47dd2a73791f342de999
ankitpateriya16 <ankitpateriya161008@gmail.com>
2025-06-17 22:35:37 +0530

    Update setup-aws.sh

--- setup-aws.sh
+++ setup-aws.sh
@@ -1,4 +1,4 @@
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-apt install unzip
-unzip awscliv2.zip
+sudo apt install unzip
+sudo unzip awscliv2.zip
 sudo ./aws/install
